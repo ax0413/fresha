860543a76b6a41fc68dbe567cb9a6067d286ef9c
haein <bmiracolo@gmail.com>
2015-11-18 17:21:40 +0000

    Basic design

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :loo  
+  resources :loos
   
   get ':controller(/:action(/:id))'
   post ':controller(/:action(/:id))'
 
-  root 'loo#index'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
