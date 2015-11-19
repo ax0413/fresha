f850f10677eac910c950acc69f032884bb237e71
jeon4933 <jeon4933@snu.ac.kr>
2015-11-19 03:41:48 +0000

    asd

--- db/migrate/20151119032647_create_buildings.rb
+++ db/migrate/20151119032647_create_buildings.rb
@@ -1,7 +1,7 @@
 class CreateBuildings < ActiveRecord::Migration
   def change
     create_table :buildings do |t|
-
+      t.integer :college_id
       t.timestamps null: false
     end
   end
--- db/migrate/20151119032735_create_wcs.rb
+++ db/migrate/20151119032735_create_wcs.rb
@@ -1,7 +1,8 @@
 class CreateWcs < ActiveRecord::Migration
   def change
     create_table :wcs do |t|
-
+      t.integer :college_id
+      t.integer :building_id
       t.timestamps null: false
     end
   end
